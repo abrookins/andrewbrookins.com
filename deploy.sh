d610cc7b616dbffbf4f572872aed7986badcdd06
Andrew Brookins <a.m.brookins@gmail.com>
2016-12-22 22:48:10 -0800

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jekyll build && rsync -v -rz --checksum --delete _site/ andrewbrookins.com:_site && ssh -t andrewbrookins.com 'sudo rm -rf /var/www/jekyll && sudo cp -r _site /var/www/jekyll && sudo chown -R www-data:www-data /var/www/jekyll'
