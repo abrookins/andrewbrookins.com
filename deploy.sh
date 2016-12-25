fdca2c16e522caac536a08bf183ceb31fb7fb409
Andrew Brookins <a.m.brookins@gmail.com>
2016-12-24 16:20:48 -0800

    Deploy to domain; fix analytics

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-jekyll build && rsync -v -rz --checksum --delete _site/ andrewbrookins.com:_site && ssh -t andrewbrookins.com 'sudo rm -rf /var/www/jekyll && sudo cp -r _site /var/www/jekyll && sudo chown -R www-data:www-data /var/www/jekyll'
+#jekyll build && rsync -v -rz --checksum --delete _site/ andrewbrookins.com:_site && ssh -t andrewbrookins.com 'sudo rm -rf /var/www/jekyll && sudo cp -r _site /var/www/jekyll && sudo chown -R www-data:www-data /var/www/jekyll'
+JEKYLL_ENV=production bundle exec jekyll build \
+    && rsync -v -rz --checksum --delete _site/ andrew@andrewbrookins.com:_site \
+    && ssh -t andrew@andrewbrookins.com 'sudo rm -rf /var/www/jekyll && sudo cp -r _site /var/www/jekyll && sudo chown -R www-data:www-data /var/www/jekyll'
